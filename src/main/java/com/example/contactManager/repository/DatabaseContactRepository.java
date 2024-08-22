a4c8f9c926cd073b2c4b3c79c7a87574c8633a8a
Dmytro Korol <dimakorol77777@gmail.com>
2024-08-23 01:20:11 +0300

    add new CRUD Spring Project with Authorization

--- src/main/java/com/example/contactManager/repository/DatabaseContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.contactManager.repository;
-
-import com.example.contactManager.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DatabaseContactRepository extends JpaRepository<Contact, Integer>, ContactRepository {
-}
--- src/main/java/com/example/contactManager/service/UserService.java
+++ src/main/java/com/example/contactManager/service/UserService.java
@@ -17,12 +17,22 @@ public class UserService {
     private UserRepository userRepository;
 
     public User register(RegisterRequestDto request) {
+        // Проверяем, существует ли уже пользователь с таким email
+        Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
+        if (existingUser.isPresent()) {
+            throw new RuntimeException("User with this email already exists");
+        }
+
+        // Если пользователь с таким email не найден, создаем нового
         User user = new User();
         user.setEmail(request.getEmail());
         user.setPassword(request.getPassword()); // Пароль должен быть захеширован
         user.setName(request.getName());
+
+        // Сохраняем нового пользователя в базе данных
         return userRepository.save(user);
     }
+
 
     public LoginResponseDto login(LoginRequestDto request) {
         Optional<User> userOptional = userRepository.findByEmail(request.getEmail());
@@ -30,7 +40,7 @@ public class UserService {
             String token = generateToken(userOptional.get());
             return new LoginResponseDto(request.getEmail(), token);
         }
-        throw new RuntimeException("Invalid credentials");
+        throw new RuntimeException("Invalid Login or Password");
     }
 
     private String generateToken(User user) {
